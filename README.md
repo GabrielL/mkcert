65124feed28610531bc7465238f22591530f94e0
Gabriel Laskar <gabriel@lse.epita.fr>
2017-05-04 18:30:29 +0200

    add basic support
    Signed-off-by: Gabriel Laskar <gabriel@lse.epita.fr>

---
+++ README.md
@@ -0,0 +1,6 @@
+mkcert - Self Signed certificates made easy
+===========================================
+
+	$ mkcert www.example.com
+	$ ls
+	www.example.com-key.pem www.example.com-cert.pem
--- mkcert/__init__.py
+++ mkcert/__init__.py
@@ -1,6 +1,24 @@
-def main():
-    pass
+import argparse
+import subprocess
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+def main():
+    parser = argparse.ArgumentParser(description='mkcert')
+    parser.add_argument('domains', nargs='+')
+
+    args = parser.parse_args()
+
+    for domain in args.domains:
+        command = [
+            "openssl", "req", "-x509", "-newkey", "rsa:4096", "-days", "365",
+            "-sha256", "-nodes",
+            "-keyout", "{}-key.pem".format(domain),
+            "-out", "{}-cert.pem".format(domain),
+            "-subj", "/C=FR/CN={}".format(domain),
+        ]
+
+        subprocess.check_call(command)
